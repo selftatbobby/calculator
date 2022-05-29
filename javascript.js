8d9b9624574add0946efe6b9d88fc2fcb85c9ecb
Bobby Tat <messaroundacc1@hotmail.com>
2022-05-29 13:48:50 -0700

    added more functions

--- javascript.js
+++ javascript.js
@@ -1,16 +1,27 @@
 function add(a,b) {
-    return a + b;
+    return a + b
 }
 
 function subtract(a,b) {
-    return a - b;
+    return a - b
 }
 
 function multiply(a,b) {
-    return a * b;
+    return a * b
 }
 
 function divide(a,b) {
-    return a / b;
+    return a / b
+}
+
+function operate(operator, a, b) {
+    a = Number(a)
+    b = Number(b)
+    if (operator == '+') {
+        return add(a,b)
+    } else if (operator == '-') {
+        return subtract(a,b)
+    } else if
+ 
 }
 
